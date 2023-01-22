44ac02f03f1d1061d10085f1389e5a25e09c40ba
Anastasiia Yavtushenko <Icenn85@gmail.com>
2023-01-22 22:21:44 +0200

    add singup in controllers

--- controllers/user/signup.js
+++ controllers/user/signup.js
@@ -3,13 +3,21 @@ const { HttpError } = require("../../helpers/index");
 const bcrypt = require("bcrypt");
 
 async function signup(req, res, next) {
-  try {
-      const { email, password } = req.body;
-      const salt = await bcrypt.genSalt();
-
-  
-    return res.status(201).json(updatedStatusContact);
+    try {
+        const { email, password } = req.body;
+        const salt = await bcrypt.genSalt();
+        const hashedPass = await bcrypt.hash(password, salt);
+        const savedUser = await User.create({ email, password: hashedPass });
+    return res.status(201).json({
+      user: {
+        email,
+        subscription: savedUser.subscription,
+      },
+    });
   } catch (error) {
+      if (error.message.includes("E11000 duplicate key error")) {
+        return next(HttpError(409, "User with this email already exists"));
+      }
     next(error);
   }
 }
